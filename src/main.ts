03cec36a875a9ef079fb9dddd5e5a47aa8764001
Akiicat <aaaa1379@gmail.com>
2018-12-05 14:06:11 +0800

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 
-const testFolder = '/cloudsql/';
-const fs = require('fs');
+try {
+  const testFolder = '/cloudsql/';
+  const fs = require('fs');
 
-console.log("---cloudsql----")
-fs.readdirSync(testFolder).forEach(file => {
-  console.log(file);
-})
+  console.log("---cloudsql----")
+  fs.readdirSync(testFolder).forEach(file => {
+    console.log(file);
+  })
+}
+catch {
+  console.log("---cloudsql error----")
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
