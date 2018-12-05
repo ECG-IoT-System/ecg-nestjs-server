d17d07ff3f181044e6fa4bbbb47891ce72ac507c
Akiicat <aaaa1379@gmail.com>
2018-12-05 11:58:14 +0800

    check cloudsql folder

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 
+const testFolder = './tests/';
+const fs = require('fs');
+
+fs.readdir('/cloudsql', (err, files) => {
+  files.forEach(file => {
+    console.log(file);
+  });
+})
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
