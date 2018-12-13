9e87482e2632e8bdf496e2c9eab3824eaf58c0c1
pei0307 <annie830307@gmail.com>
2018-12-14 01:15:46 +0800

    code format

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -9,7 +9,7 @@ import { Mac } from '../macs/mac.entity'
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>,   
+    private readonly userRepository: Repository<User>,
     @InjectRepository(Mac)
     private readonly macRepository: Repository<Mac>,
   ) { }
@@ -27,35 +27,35 @@ export class UserService {
   }
 
   async createOne(user): Promise<User> {
-    let Isuser = await this.userRepository.findOne({ username : user.username });
+    let Isuser = await this.userRepository.findOne({ username: user.username });
     console.log(Isuser);
-    if (!Isuser) { 
+    if (!Isuser) {
       return await this.userRepository.save(user);
     }
   }
 
   async updateLasttime(param) {
-    let user = await this.userRepository.findOne({id : param.id});
+    let user = await this.userRepository.findOne({ id: param.id });
     let query: any = {}
 
-    if(param.lasttime && param.lasttime > user.lasttime){
+    if (param.lasttime && param.lasttime > user.lasttime) {
       // return minimum mac lasttime 
       var minlasttime = await this.macRepository.createQueryBuilder("mac")
-      .select("min(mac.lasttime)","mintime")
-      .where({user:param.id, status:true}).execute();
-      console.log(param.lasttime , minlasttime[0].mintime);
-      if(minlasttime[0].mintime >= user.lasttime)
+        .select("min(mac.lasttime)", "mintime")
+        .where({ user: param.id, status: true }).execute();
+      console.log(param.lasttime, minlasttime[0].mintime);
+      if (minlasttime[0].mintime >= user.lasttime)
         query.lasttime = minlasttime[0].mintime;
     }
 
-    else if(param.lasttime_12L && param.lasttime_12L > user.lasttime_12L){
+    else if (param.lasttime_12L && param.lasttime_12L > user.lasttime_12L) {
       query.lasttime_12L = param.lasttime_12L;
     }
 
-    else if(param.lasttime_afstat && param.lasttime_afstat > user.lasttime_afstat){
+    else if (param.lasttime_afstat && param.lasttime_afstat > user.lasttime_afstat) {
       query.lasttime_afstat = param.lasttime_afstat;
-    }  
-    return await this.userRepository.update({ id :param.id }, query)
+    }
+    return await this.userRepository.update({ id: param.id }, query)
   }
 
 }
