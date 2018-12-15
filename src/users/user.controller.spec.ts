c6509757d772394493c9c8cda7bbbf6e25a47306
Akiicat <aaaa1379@gmail.com>
2018-12-15 16:12:08 +0800

    remove user controller spec

--- src/users/user.controller.spec.ts
+++ src/users/user.controller.spec.ts
@@ -1,31 +1,31 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
-import { UserModule } from './user.module'
-import { AppModule } from '../app.module'
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user.entity';
-import { Ecgdata } from '../ecgdata/ecgdata.entity';
-import { Ecgdata12 } from '../ecgdata12/ecgdata12.entity';
-import { Gsensor } from '../gsensor/gsensor.entity';
-import { Mac } from '../macs/mac.entity';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { UserController } from './user.controller';
+// import { UserService } from './user.service';
+// import { UserModule } from './user.module'
+// import { AppModule } from '../app.module'
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { User } from './user.entity';
+// import { Ecgdata } from '../ecgdata/ecgdata.entity';
+// import { Ecgdata12 } from '../ecgdata12/ecgdata12.entity';
+// import { Gsensor } from '../gsensor/gsensor.entity';
+// import { Mac } from '../macs/mac.entity';
 
-describe('UserController', () => {
-    let module: TestingModule;
-    let userController: UserController;
+// describe('UserController', () => {
+//     let module: TestingModule;
+//     let userController: UserController;
 
-    beforeAll(async () => {
-        module = await Test.createTestingModule({
-            imports: [AppModule],
-        }).compile();
-        userController = module.get<UserController>(UserController);
-    });
+//     beforeAll(async () => {
+//         module = await Test.createTestingModule({
+//             imports: [AppModule],
+//         }).compile();
+//         userController = module.get<UserController>(UserController);
+//     });
 
-    describe('findAll', () => {
-        it('get all', async () => {
-            console.log('---', userController)
-            expect(userController).toBeDefined();
-            // expect(await userController.findAll()).toBe('Hello World!');
-        });
-    });
-});
+//     describe('findAll', () => {
+//         it('get all', async () => {
+//             console.log('---', userController)
+//             expect(userController).toBeDefined();
+//             // expect(await userController.findAll()).toBe('Hello World!');
+//         });
+//     });
+// });
